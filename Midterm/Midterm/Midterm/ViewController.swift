7562567fa37ce3ce83df6bc0135faebde68baf39
Cady <cadyspeelman@cu-engr2-2-1-10.201.67.112.int.colorado.edu>
2019-10-15 13:22:02 -0600

    midterm functional

--- Midterm/Midterm/Midterm/ViewController.swift
+++ Midterm/Midterm/Midterm/ViewController.swift
@@ -22,15 +22,21 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     
     
-    @IBAction func changeTransport(_ sender: UISegmentedControl) {
+    @IBAction func changeTransport(_ sender: UISegmentedControl) {//car
         if transportSegControl.selectedSegmentIndex == 0 {
             transportImg.image=UIImage(named: "car_icon")
-        }
-        else if (transportSegControl.selectedSegmentIndex == 1 ){
+        }else if (transportSegControl.selectedSegmentIndex == 1 ){//bus
             transportImg.image=UIImage(named: "bus_icon")
-        }else if (transportSegControl.selectedSegmentIndex == 2){
-            
+            reportGasNeeded.text = "0 gallons"
+                let alert = UIAlertController(title: "Info", message: "A 10 minute wait time is added to total commute time for busing", preferredStyle: UIAlertController.Style.alert)
+                let cancelAction = UIAlertAction(title: "Okay", style: UIAlertAction.Style.cancel, handler: nil)
+                alert.addAction(cancelAction)
+                present(alert, animated: true, completion: nil)
+        }else if (transportSegControl.selectedSegmentIndex == 2){//bike
+            transportImg.image=UIImage(named: "bike_icon")
+            reportGasNeeded.text = "0 gallons"
         }
+        updateCommute()
     }
     
     @IBAction func tankSlider(_ sender: UISlider) {
@@ -41,6 +47,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBAction func commuteButton(_ sender: Any) {
         updateCommute()
     }
+    
     @IBAction func monthlySwitch(_ sender: Any) {
         //20 work days, multiply totals by 20
         updateCommute()
@@ -48,42 +55,68 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     
     func updateCommute(){
-        
-        //time= distance/speed (avg 20 mi/hr)
-        if(commuteMiles.text!.isEmpty){ //no input, default to zero
-            reportCommuteTime.text = "0 mins"
-        }else{ //data is present
-            var distance = Float(commuteMiles.text!)!
-            if(monthlySwitch.isOn){ // switch on
-                distance = distance*20 //20 work days, multiply totals by 20
-                reportCommuteTime.text = String(format: "%.2f", distance/(20/60))+" mins"
-            }else{//switch is off
-                if(distance==(20/60)){ // 20 mi/hr = .33mi/min
-                    reportCommuteTime.text = "1 min"
-                }else{ //all others
+        //if car
+        if transportSegControl.selectedSegmentIndex == 0 {
+            //time= distance/speed (avg 20 mi/hr)
+            if(commuteMiles.text!.isEmpty){ //no input, default to zero
+                reportCommuteTime.text = "0 mins"
+            }else{ //data is present
+                var distance = Float(commuteMiles.text!)!
+                if(monthlySwitch.isOn){ // switch on
+                    distance = distance*20 //20 work days, multiply totals by 20
                     reportCommuteTime.text = String(format: "%.2f", distance/(20/60))+" mins"
-                }//redundant, fix if time allows
+                }else{//switch is off
+                    if(distance==(20/60)){ // 20 mi/hr = .33mi/min
+                        reportCommuteTime.text = "1 min"
+                    }else{ //all others
+                        reportCommuteTime.text = String(format: "%.2f", distance/(20/60))+" mins"
+                    }//redundant, fix if time allows
+                }
+            }
+            //gas = mi/gal (avg 24 mi/gallon)
+            //gallons needed = distance/24
+            if(commuteMiles.text!.isEmpty){ //no input, default to zero
+                reportGasNeeded.text = "0 gallons"
+            }else{ //data is present
+                var distance = Float(commuteMiles.text!)!
+                if(monthlySwitch.isOn){ // switch on
+                    distance = distance*20
+                }//do for both on and off switch
+                if(distance==24){ // 24 miles takes 1 gallon
+                    reportGasNeeded.text = "1 gallon"
+                }else{ //all others
+                    reportGasNeeded.text = String(format: "%.2f", distance/24)+" gallons"
+                }
+            }
+        }//end car
+        else if(transportSegControl.selectedSegmentIndex == 1){//if bus
+                //time= distance/speed (avg 12 mi/hr) +5
+            if(commuteMiles.text!.isEmpty){ //no input, default to zero
+                    reportCommuteTime.text = "0 mins"
+            }else{ //data is present
+                var distance = Float(commuteMiles.text!)!
+                if(monthlySwitch.isOn){ // switch on
+                    distance = distance*20 //20 work days
+                    reportCommuteTime.text = String(format: "%.2f", distance/(12/60) + 200 )+" mins" //plus 20*10 waiting mins
+                }else{
+                reportCommuteTime.text = String(format: "%.2f", distance/(12/60) + 10 )+" mins"
+                }
+            }
+        }else if(transportSegControl.selectedSegmentIndex == 2){//if bike
+            //time= distance/speed (avg 10 mi/hr)
+            if(commuteMiles.text!.isEmpty){ //no input, default to zero
+                reportCommuteTime.text = "0 mins"
+            }else{ //data is present
+                var distance = Float(commuteMiles.text!)!
+                if(monthlySwitch.isOn){ // switch on
+                    distance = distance*20 //20 work days
+                }
+                reportCommuteTime.text = String(format: "%.2f", distance/(10/60))+" mins"
             }
         }
+
         
-        //gas = mi/gal (avg 24 mi/gallon)
-        //gallons needed = distance/24
-        if(commuteMiles.text!.isEmpty){ //no input, default to zero
-            reportGasNeeded.text = "0 gallons"
-        }else{ //data is present
-            var distance = Float(commuteMiles.text!)!
-            if(monthlySwitch.isOn){ // switch on
-                distance = distance*20
-            }//do for both on and off switch
-            if(distance==24){ // 24 miles takes 1 gallon
-                reportGasNeeded.text = "1 gallon"
-            }else{ //all others
-                reportGasNeeded.text = String(format: "%.2f", distance/24)+" gallons"
-            }
-            
         }
-        
-    }
     
     func textFieldShouldReturn(_ textField: UITextField)-> Bool{
         textField.resignFirstResponder()

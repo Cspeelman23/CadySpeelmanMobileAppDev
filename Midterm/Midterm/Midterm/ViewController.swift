4a8b8a67940dd61f0d06532c3ef478936f1959de
Cady <cadyspeelman@cu-engr2-2-1-10.201.67.112.int.colorado.edu>
2019-10-15 12:28:09 -0600

    midterm more progress

--- Midterm/Midterm/Midterm/ViewController.swift
+++ Midterm/Midterm/Midterm/ViewController.swift
@@ -15,12 +15,26 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var reportCommuteTime: UILabel!
     @IBOutlet weak var reportGasNeeded: UILabel!
     
+    @IBOutlet weak var gallonsInTank: UILabel!
+    
+    
+    
+    
+    @IBAction func tankSlider(_ sender: UISlider) {
+        let tankVal=sender.value //float
+        gallonsInTank.text=String(format: "%.2f", tankVal)+"gallons"
+    }
+    
+    @IBAction func commuteButton(_ sender: Any) {
+        updateCommute()
+    }
+    @IBAction func monthlySwitch(_ sender: Any) {
+    }
     
     
     func updateCommute(){
         
-        //time= distance/speed
-        
+        //time= distance/speed (avg 20 mi/hr)
         if(commuteMiles.text!.isEmpty){ //no input, default to zero
             reportCommuteTime.text = "0 mins"
         }else{
@@ -28,8 +42,21 @@ class ViewController: UIViewController, UITextFieldDelegate {
             if(distance==(20/60)){ // 20 mi/hr = .33mi/min
                     reportCommuteTime.text = "1 min"
                 }else{ //all others
-                    reportCommuteTime.text = String(distance/(20/60))+" mins"
+                    reportCommuteTime.text = String(format: "%.2f", distance/(20/60))+" mins"
                 }
+        }
+        
+        //gas = mi/gal (avg 24 mi/gallon)
+        //gallons needed = distance/24
+        if(commuteMiles.text!.isEmpty){ //no input, default to zero
+            reportGasNeeded.text = "0 gallons"
+        }else{
+            let distance = Float(commuteMiles.text!)!
+            if(distance==24){ // 24 miles takes 1 gallon
+                reportGasNeeded.text = "1 gallon"
+            }else{ //all others
+                reportGasNeeded.text = String(format: "%.2f", distance/24)+" gallons"
+            }
             
         }
         

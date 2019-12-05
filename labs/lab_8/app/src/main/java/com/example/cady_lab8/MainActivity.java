291340970e7fcc3c663b26d0f03dc383474b2bda
Cady <cadyspeelman@atlasfritz-22.local>
2019-12-05 00:27:06 -0700

    lab 8 functional

--- labs/lab_8/app/src/main/java/com/example/cady_lab8/MainActivity.java
+++ labs/lab_8/app/src/main/java/com/example/cady_lab8/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cady_lab8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,45 +16,49 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup typeSelect;
     private TravelType myTravelType = new TravelType();
     private TextView testy;
-
+    public Integer Ttype = 0;
     public void onRadioButtonClicked(View view){
         boolean picked = ((RadioButton)view).isChecked();
-        String Ttype = "0";
+
         switch(view.getId()) {
             case R.id.radioButton:
                 if (picked) {
-                    Ttype = "0";
+                    Ttype = 0;
                 }
                 break;
             case R.id.radioButton2:
                 if (picked) {
-                    Ttype = "1";
+                    Ttype = 1;
                 }
                 break;
             case R.id.radioButton3:
                 if (picked) {
-                    Ttype = "2";
+                    Ttype = 2;
                 }
                 break;
             default:
-                Ttype = "0";
+                Ttype = 0;
         }
 
-        testy.setText(Ttype);
+       // testy.setText(Ttype);
     }
 
     private void findTransport(View view){
         //get radio selection
-        int radioSelect = typeSelect.getCheckedRadioButtonId();
+        //int radioSelect = typeSelect.getCheckedRadioButtonId();
         //set result
-        myTravelType.setTransportMode(radioSelect);
+        myTravelType.setTransportMode(Ttype);
         //get result
         String suggestedTransport = myTravelType.getTransportMode();
         //get url
         String suggestTransportURL = myTravelType.getSiteURL();
-        Log.i("suggested type","suggestedTransport");
-        Log.i("suggested URL","suggestTransportURL");
+        Log.i("radioSelect", Integer.toString(Ttype));
 
+        //Intent
+        Intent intent = new Intent(this, TransportationActivity.class);
+        intent.putExtra("travelType",suggestedTransport);
+        intent.putExtra("typeURL",suggestTransportURL);
+        startActivity(intent);
     }
 
     @Override
@@ -62,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
         typeSelect = (RadioGroup)findViewById(R.id.radioGroup);
-        testy = findViewById(R.id.testBox);
+        Ttype = 0;
         //listener
         View.OnClickListener onclick = new View.OnClickListener() {
             public void onClick(View view) {
--- labs/lab_8/app/src/main/java/com/example/cady_lab8/TransportationActivity.java
+++ labs/lab_8/app/src/main/java/com/example/cady_lab8/TransportationActivity.java
@@ -1,5 +1,7 @@
 package com.example.cady_lab8;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,9 +10,13 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class TransportationActivity extends AppCompatActivity {
+    private String travelType;
+    private String typeURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +28,23 @@ public class TransportationActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+            public void onClick(View view) {loadWebSite(view);    }
         });
+
+        Intent intent = getIntent();
+        travelType = intent.getStringExtra("travelType");
+        typeURL = intent.getStringExtra("typeURL");
+        Log.i("type received", travelType);
+        Log.i("URL received", typeURL);
+
+        //update text view
+        TextView messageView = findViewById(R.id.resultView);
+        messageView.setText("You might want to travel by " + travelType + " proof of string:" + typeURL);
+    }
+    private void loadWebSite(View view){
+        Intent intent = new Intent((Intent.ACTION_VIEW));
+        intent.setData(Uri.parse(typeURL));
+        startActivity(intent);
     }
 
 }

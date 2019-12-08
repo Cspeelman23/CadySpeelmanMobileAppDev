f5641ba3f8db701bf7477d7e443a6ee6984f1580
Cady <cadyspeelman@atlasfritz-22.local>
2019-12-08 15:06:46 -0700

    landscape and reading seekBar

--- project_2/CadyProject2/app/src/main/java/com/example/cadyproject2/MainActivity.java
+++ project_2/CadyProject2/app/src/main/java/com/example/cadyproject2/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.cadyproject2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar sauceSeekBar;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SeekBar sauceSeekBar = (SeekBar)findViewById(R.id.seekBar);
+        sauceSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressValueSauce = 0;
+            public void onProgressChanged(SeekBar sauceSeekBar, int progress, boolean fromUser) {
+
+                progressValueSauce = progress;
+                Log.i("progress", Integer.toString(progressValueSauce));
+            }
+            public void onStartTrackingTouch(SeekBar seekBar){
+              //required abstract method
+            };
+            public void onStopTrackingTouch(SeekBar seekBar){
+                //required abstract method
+
+            };
+        });
     }
 }
